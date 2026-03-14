3f9b9dd3f9e4efd7f2ab7a8e01219a64cd772ddc
Imethma Methusari Yapa Bandara <imy@Imethmas-MacBook-Air.local>
2026-03-14 16:23:48 +0530

    feat(user-profile): create initial UserProfileScreen skeleton with Scaffold

--- lib/screens/user/user_profile_screen.dart
+++ lib/screens/user/user_profile_screen.dart
@@ -1,1 +1,16 @@
-// user profile
+
+import 'package:flutter/material.dart';
+
+class UserProfileScreen extends StatelessWidget {
+  const UserProfileScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: const Center(
+        child: Text('User Profile - Step 1', style: TextStyle(fontSize: 28)),
+      ),
+    );
+  }
+}
