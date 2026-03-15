0ef77602fe9532a3643d702f1a537717c8281137
uvini-wickramaarachchi <uvini.20232398@iit.ac.lk>
2026-03-15 23:45:06 +0530

    feat: create AddGarageScreen skeleton

---
+++ lib/screens/provider/provider_add_garage_screen.dart
@@ -0,0 +1,19 @@
+// lib/screens/provider/add_garage_screen.dart
+
+import 'package:flutter/material.dart';
+
+class AddGarageScreen extends StatefulWidget {
+  const AddGarageScreen({super.key});
+
+  @override
+  State<AddGarageScreen> createState() => _AddGarageScreenState();
+}
+
+class _AddGarageScreenState extends State<AddGarageScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text('Add Garage Screen - coming soon')),
+    );
+  }
+}
