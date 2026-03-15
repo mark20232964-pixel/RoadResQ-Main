3954ec5ea0c05c3c0962330e526ca76d51e9a55e
Imethma Methusari Yapa Bandara <imy@Imethmas-MacBook-Air.local>
2026-03-15 12:42:37 +0530

    feat(verify-vehicle): add AppBar with back button and Verify Your Vehicle title

--- lib/screens/user/verify_vehicle_screen.dart
+++ lib/screens/user/verify_vehicle_screen.dart
@@ -7,6 +7,19 @@ class VerifyVehicleScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
+      appBar: AppBar(
+      backgroundColor: Colors.white,
+      elevation: 0,
+      leading: IconButton(
+        icon: const Icon(Icons.arrow_back, color: Colors.black),
+        onPressed: () => Navigator.pop(context),
+      ),
+      title: const Text(
+        "Verify Your Vehicle",
+        style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+      ),
+      centerTitle: true,
+    ),
       body: const Center(
         child: Text('Verify Vehicle Screen - Step 1', style: TextStyle(fontSize: 28)),
       ),
