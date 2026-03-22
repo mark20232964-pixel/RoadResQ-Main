aa1abbac3c2ef0478cbefc427b1c8922a592e613
Sanjana Karannagoda <sanjanakarannagoda12@gmail.com>
2026-03-22 21:26:11 +0530

    Add ServiceRequestScreen StatefulWidget structure

--- lib/screens/user/service_request_screen.dart
+++ lib/screens/user/service_request_screen.dart
@@ -5,3 +5,15 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_polyline_points/flutter_polyline_points.dart';
+
+class ServiceRequestScreen extends StatefulWidget {
+  final String initialQuery;
+
+  const ServiceRequestScreen({super.key, required this.initialQuery});
+
+  @override
+  State<ServiceRequestScreen> createState() => _ServiceRequestScreenState();
+}
+
+class _ServiceRequestScreenState extends State<ServiceRequestScreen>
+    with SingleTickerProviderStateMixin {}
