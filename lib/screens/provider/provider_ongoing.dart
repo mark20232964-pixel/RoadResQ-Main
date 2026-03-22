a6a8fefa406e48a30baf03a1dc141d3d628ec981
Sanjana Karannagoda <sanjanakarannagoda12@gmail.com>
2026-03-23 00:00:46 +0530

    chore: add required dependencies for ongoing screen

--- lib/screens/provider/provider_ongoing.dart
+++ lib/screens/provider/provider_ongoing.dart
@@ -1,4 +1,12 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart';
+
+import 'provider_request_details.dart';
 
 class ProviderOngoingScreen extends StatefulWidget {
   const ProviderOngoingScreen({super.key});
