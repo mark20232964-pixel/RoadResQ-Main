8176f9de343ec95b2cdc91af4b25aae0b52c56db
Danel <mark.20232964@iit.ac.lk>
2026-03-22 15:52:32 +0530

    feat: create empty add_service_type_screen file
    - Added initial file structure for 'Add Your Service' screen
    - Used StatelessWidget as base
    - Placeholder widget for now

---
+++ lib/screens/provider/add_service_type.dart
@@ -0,0 +1,12 @@
+// lib/screens/provider/add_service_type.dart
+
+import 'package:flutter/material.dart';
+
+class AddServiceTypeScreen extends StatelessWidget {
+  const AddServiceTypeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder(); // temporary - we'll build UI step by step
+  }
+}
